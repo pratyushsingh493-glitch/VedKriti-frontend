dd813c89585380e11df001b996113555cb766c1d
Pratyush Singh <pratyushsingh493@gmail.com>
2026-07-05 14:46:52 +0530

    Landing Page

---
+++ app.js
@@ -0,0 +1,7 @@
+const observer = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }else{
+        entries[0].target.classList.remove("show");
+    }
+},{})
