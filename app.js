793111acae2b34c270bf33a712d634f2be6411d2
Pratyush Singh <pratyushsingh493@gmail.com>
2026-07-06 18:40:45 +0530

    Full Fledged landing Page

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const observer = new IntersectionObserver((entries)=>{
-    if(entries[0].isIntersecting){
-        entries[0].target.classList.add("show");
-    }else{
-        entries[0].target.classList.remove("show");
+gsap.registerPlugin(ScrollTrigger);
+
+const container = document.querySelector('.container');
+const sections = gsap.utils.toArray('.container section');
+const text = gsap.utils.toArray('.anim');
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: "+=3000"
+  }
+});
+
+sections.forEach((section) => {
+  let text = section.querySelectorAll(".anim");
+
+  gsap.from(text, {
+    y: -130,
+    opacity: 0,
+    duration: 2,
+    ease: "elastic",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      start: "left center"
     }
-},{})
+  });
+});
