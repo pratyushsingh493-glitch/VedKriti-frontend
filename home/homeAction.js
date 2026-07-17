7e5ae4234e9c2a9c7aa579367e4d056ae52f5eeb
Pratyush Singh <pratyushsingh493@gmail.com>
2026-07-17 23:22:09 +0530

    Doctor Appointment under process

--- home/homeAction.js
+++ home/homeAction.js
@@ -7,9 +7,112 @@ const showError = (message) => {
 
 const displayDoctor = (doctor) => {
     const find = document.getElementById("find");
-    find.insertAdjacentHTML("afterbegin",`
+    find.insertAdjacentHTML("beforeend",`<div class="carousel"></div>`);
+    doctor.availablity.forEach((avl)=>{
+        document.querySelector(".carousel").insertAdjacentHTML("beforeend",`
+            <div class="date" id="${avl.date}">
+                <h3>${avl.date}</h3>
+                <button class="MORNING">Morning ${avl.morningCapacity - avl.morningBookings} slots available</button>
+                <button class="AFTERNOON">Afternoon ${avl.afternoonCapacity - avl.afternoonBookings} slots available</button>
+                <button class="EVENING">Evening ${avl.eveningCapacity - avl.eveningBookings} slots available</button>
+            </div>
+        `)
+        document.querySelectorAll(`#${avl.date} button`).forEach((button)=>{
+            const shift = button.className;
+            const patID = localStorage.getItem("userID");
+            const date = avl.date;
+            try {
+                const response = await fetch(
+                    `${domain}/api/book-doctor`,
+                    {
+                        method: "POST",
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem("token")}`
+                        },
+                        body: {
+                            patID,
+                            date,
+                            shift
+                        }
+                    }
+                );
 
-    `)
+                const data = await response.json();
+
+                if (response.ok) {
+                    alert("Booking Successful!")
+                } else {
+                    showError(data.message || "Unable to Book The slot");
+                }
+            } catch (error) {
+                showError(error.message);
+            }
+        })
+    });
+    find.insertAdjacentHTML("beforeend",`
+        <div class="card" id="${doctor.docID}">
+            <div>
+                <img src="${doctor.photo}" alt="Profile">
+            </div>
+            <div class="details">
+                <h1>${doctor.name}</h1>
+                <br>
+                <b>${doctor.degreeType} - ${doctor.degreeName} - ${doctor.fieldOfStudy}</b>
+                <b>${doctor.institute}</b>
+                <b>${doctor.specialization1}, ${doctor.specialization2}, ${doctor.specialization3}</b>
+                <br>
+                <b>${doctor.country}, ${doctor.state}, ${doctor.city}</b>
+                <p>${doctor.facilityName}</p>
+                <br>
+                <p>${doctor.address}</p>
+                <br>
+                <b>More about ${doctor.name}</b>
+                <p>${doctor.about}</p>
+            </div>
+            <div class="price">
+                Consultation Fee : 
+                &#8377;${doctor.consultationFee}
+            </div>
+        </div>
+    `);
+    find.insertAdjacentHTML("beforeend",`<div class="timeline"></div>`)
+    let idx = 0;
+    doctor.experiance.forEach((exp)=>{
+        const label = ((idx%2==0)?"left":"right");
+        idx++;
+        document.querySelector(".timeline").insertAdjacentHTML("afterbegin",`
+            <div class="container ${label}">
+                <img src="../media/point.webp">
+                <div class="text-box">
+                    <h2>${exp.facilityName}</h2>
+                    <small>${exp.startDate} - ${exp.endDate}</small>
+                    <p>${exp.designation}</p>
+                    <span class="${label}-arrow"></span>
+                </div>
+            </div>
+        `)
+    });
+    doctor.patients.forEach((patient)=>{
+        find.insertAdjacentHTML("beforeend",`
+            <div class="card" id="${patient.patID}">
+                <div>
+                    <img src="${patient.photo}" alt="Profile">
+                </div>
+                <div class="details">
+                    <h1 style="text-align: left;">${patient.name}</h1>
+                    <div class="star"></div>
+                    <br>
+                    <p>${patient.feedback}</p>
+                </div>
+            </div>
+        `)
+
+        for(let i=0;i<5;i++){
+            let c = "n";
+            if(i<patient.rating) c = "y";
+            document.querySelector(`#${patient.patID} .star`).insertAdjacentHTML("beforeend",`<img src="../media/${c}star.png">`)
+        }
+    })
 }
 
 const addResults = (details) => {
@@ -22,11 +125,16 @@ const addResults = (details) => {
             <div class="details">
                 <h1>${details.name}</h1>
                 <br>
-                <b>${details.speciality1}, ${details.speciality2}, ${details.specialit3}</b>
+                <b>${details.degreeType} - ${details.degreeName} - ${details.fieldOfStudy}</b>
+                <b>${details.institute}</b>
+                <b>${details.specialization1}, ${details.specialization2}, ${details.specialization3}</b>
                 <br>
                 <b>${details.country}, ${details.state}, ${details.city}</b>
                 <p>${details.facilityName}</p>
                 <br>
+                <p>${details.address}</p>
+                <br>
+                <b>More about ${details.name}</b>
                 <p>${details.about}</p>
             </div>
             <div class="price">
@@ -36,10 +144,10 @@ const addResults = (details) => {
         </div>
     `);
 
-    document.getElementById(`${details.docID}`).addEventListener("click",()=>{
+    document.getElementById(`${details.docID}`).addEventListener("click",async()=>{
         try {
             const response = await fetch(
-                `${domain}/api/fetch-doctor?id=${localStorage.getItem("docID")}`,
+                `${domain}/api/fetch-doctor?id=${encodeURIComponent(details.docID)}`,
                 {
                     method: "GET",
                     headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
@@ -50,7 +158,7 @@ const addResults = (details) => {
 
             if (response.ok) {
                 document.getElementById("filter").remove();
-                document.getElementById("searchResult").remove();
+                document.getElementById("searchResults").remove();
                 document.querySelector("title").innerText = data.name;
                 displayDoctor(data);
             } else {
